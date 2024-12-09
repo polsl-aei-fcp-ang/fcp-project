fea05fe570bbb27b6a7865b1a140f4b1931b64d0
ksiminski <ksiminski@polsl.pl>
2024-12-09 15:32:49 +0100

    the idea of the main

--- source/main.cpp
+++ source/main.cpp
@@ -1,8 +1,22 @@
 
+#include <string>
 
-
-int main ()
+int main (int number_params, char * params[])
 {
+    // std::pair<std::string,std::string> input_output_names = read_names_of_files (number_params, params);
+
+    // if (not input_output_names.first.empty() and not input_output_names.second.empty())
+    // {
+        // OK
+        // graph conflicts = read_conflicts(input_output_names.first);
+        // colour_nodes(conflicts);
+        // print_colours(conflicts, input_output_names.second);
+
+    // }
+    // else
+    // {
+        // help();
+    // }
 
     return 0;
 }
